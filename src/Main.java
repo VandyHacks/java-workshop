6c404d599ded222a511760024110fdeee1204e2d
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-10-09 15:49:58 -0500

    Made Main extend Game

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,39 @@
 /**
- * @author - <type your name here>
+ * TODO - Add your name below next to the @author tag!
+ * @author
  * <p>
  * This class runs your Battleship game!
  */
 
-public class Main {
+import core.*;
 
+public class Main extends Game {
     public static void main(String[] args) {
+
+    }
+    
+    @Override
+    public String getObjective() {
+        return null;
+    }
+
+    @Override
+    public Arena initializeArena() {
+        return null;
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return false;
+    }
+
+    @Override
+    public String getResults() {
+        return null;
+    }
+
+    @Override
+    public void run() {
 
     }
 }
