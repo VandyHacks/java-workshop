2696716a516f949a2654337220ffcc3791d07872
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-10-10 14:18:08 -0500

    update gitignore

--- battleship/ships/PatrolShip.java
+++ battleship/ships/PatrolShip.java
@@ -1,27 +1,12 @@
-package battleship.ships;
-import battleship.core.*;
-import java.util.List;
+/*
+ * Irfaan Khalid
+ * 10/10/2018
+ *
+ * Description:
+ */
 
-public class PatrolShip extends Ship {
-    
-    public PatrolShip() {
-        this.initializeName("Patrol Ship");
-        this.initializeOwner("The Evil Fleet");
-        this.initializeHull(6);
-        this.initializeFirepower(1);
-        this.initializeSpeed(1);
-        this.initializeRange(2);
-    }
-    
-    @Override
-    protected void doTurn(Arena arena) {
-        this.move(arena, Direction.WEST);
-        List<Ship> nearby = this.getNearbyShips(arena);
-        if (nearby.size() > 0) {
-            Ship target = nearby.get(0);
-            Coord coord = target.getCoord();
-            this.fire(arena, coord.getX(), coord.getY());
-        }
-    }
-    
+package battleship.ships;
+
+public class PatrolShip {
+
 }
