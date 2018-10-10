6fd94251edfb99dc14142f476dc3ea9cbd494149
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-10-10 15:18:42 -0500

    removing viz scripts

--- viz.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-MISSION_ID="duobattle"
-BATTLE_SEED=27
-echo "import $MISSION_ID.*;
-import $MISSION_ID.Main;" >MISSION_HEADER
-cat MISSION_HEADER RawDavyJonesLocker.java > DavyJonesLocker.java
-javac -cp ".:lib/*" DavyJonesLocker.java 2>>DEBUG
-java -cp ".:lib/*" DavyJonesLocker "$BATTLE_SEED" >>DEBUG 2>&1
-curl --data "@results.json" https://mimirbattleships.glitch.me/data --header "Content-Type: application/json" >DEBUG
