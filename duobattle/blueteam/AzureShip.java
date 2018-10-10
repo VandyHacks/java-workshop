e260601a082e79dbcfd1ae999527205e581b113a
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-10-10 14:29:27 -0500

    done with AzureShip todo

--- duobattle/blueteam/AzureShip.java
+++ duobattle/blueteam/AzureShip.java
@@ -1,27 +1,35 @@
 package duobattle.blueteam;
 
 import battleship.core.Arena;
+import battleship.core.Coord;
+import battleship.core.Direction;
 import battleship.core.Ship;
+import java.util.List;
 
 /**
  * Blue Team: Azure Ship
+ * This will be a basic patrol ship!
  *
  * @author TODO - Add your name!
  */
 public class AzureShip extends Ship {
-
     /**
      * Constructor
      */
     public AzureShip() {
-        this.initializeName("Azure");
-        this.initializeOwner("Your Name"); // TODO - Change this to your name!
+        /*
+         * Let's build our constructor, the method that creates our ship.
+         */
 
-        // TODO - Give your ship up to 10 points total for its hull, firepower, speed, and range
-        this.initializeHull(1);
-        this.initializeFirepower(1);
-        this.initializeSpeed(1);
-        this.initializeRange(1);
+        // TODO - Our ship needs a name and an owner
+        this.initializeName("");
+        this.initializeOwner("");
+
+        // TODO - Our ship's scores must add up to 10 at most. Let's give it some points!
+        this.initializeHull(-1); // Let's make this 6
+        this.initializeFirepower(-1); // Let's make this 1
+        this.initializeSpeed(-1); // Let's make this 1
+        this.initializeRange(-1); // Let's make this 2
     }
 
     /**
@@ -31,7 +39,15 @@ public class AzureShip extends Ship {
      */
     @Override
     protected void doTurn(Arena arena) {
-        // TODO - Fill this in!
+        /*
+         * Let's build a basic patrol ship.
+         */
+
+        // TODO - Let's make the ship move WEST on every turn
+
+        // TODO - Next, let's obtain all nearby ships in the arena
+
+        // TODO - Now, if there are nearby enemy ships, let's target them and fire at them
     }
 
 }
